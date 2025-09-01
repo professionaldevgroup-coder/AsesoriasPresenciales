c4c7c91661e186309286b30f82ef580708e05672
JuanFe Dev Master <professional.dev.group@gmail.com>
2025-09-01 12:00:31 -0500

    First Commit

---
+++ VisualStudioProjects/WebApiExample/WebApiExample/Controllers/VehicleController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiExample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VehicleController : ControllerBase
+    {
+        [HttpPost]
+        [Route("CreateVehicule")]
+        public IActionResult CreateVehicule(string brand,
+            int doorAmout,
+            int amountPassagers,
+            int year,
+            string model,
+            string color)
+        {
+
+            return StatusCode(500,new { message = "ServerError" });
+
+        }
+    }
+}
